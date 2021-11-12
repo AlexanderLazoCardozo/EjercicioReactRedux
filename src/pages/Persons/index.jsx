2d08ed4151bb39836aef3812fbfc7db5564ab7b5
AlexanderLazoCardozo <lazoalexander86@gmail.com>
2021-11-12 12:36:25 -0500

    EjercicioReactReduxAlumnoLazo

--- src/pages/Persons/index.jsx
+++ src/pages/Persons/index.jsx
@@ -24,7 +24,7 @@ const Persons = () => {
       <ul>
         {!person.loading && person.data.length > 0 ?
           person.data.map((d) => (
-            <li>{`${d.nombres} `}</li>
+            <li>{`${d.nombres} ${d.apellidos}`}</li>
           ))
         : <h4>Loading ..!</h4>}
       </ul>
